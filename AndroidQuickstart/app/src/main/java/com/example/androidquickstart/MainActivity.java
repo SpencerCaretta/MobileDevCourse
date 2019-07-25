39d228bf397227589b644281325a9e3062006883
Spencer Caretta <spencer.caretta@gmail.com>
2019-07-25 15:01:28 -0500

    Initial commit

---
+++ AndroidQuickstart/app/src/main/java/com/example/androidquickstart/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidquickstart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void confirmName (View view) {
+        Intent confirmNameIntent = new Intent(this, ConfirmNameActivity.class);
+        EditText nameText = (EditText) findViewById(R.id.activity_main_name);
+        String name = nameText.getText().toString();
+        confirmNameIntent.putExtra("name", name);
+        startActivity(confirmNameIntent);
+    }
+}
